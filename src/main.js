ff5c52ac397bd29c295d1f67b54b7c8849e43215
Evan Kysley <e.kysley@gmail.com>
2017-02-26 23:50:27 -0500

    npm install

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import VueResource from 'vue-resource'
+
+// Vue.use(VueResource)
+
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
+
+// https://github.com/atomiks/reddit-user-analyser/tree/master/src/components
